e25f1b0c6927389d0933ecf4da99f736cfaae79f
Frankdias92 <frankmcdias@gmail.com>
2023-10-31 22:55:17 -0300

    :truck: update

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,20 +3,29 @@ const volumeSlider = document.querySelector(".volume-slider input");
 const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
-let audio = new Audio("src/tunes/a.wav");
-
-const playTune = (key) => {
-  audio.src = `src/tunes/${key}.wav`;
-  audio.play();
-
-  const clickedKey = document.querySelector(`[data-key="${key}"]`);
-  clickedKey.classList.add("active");
-  setTimeout(() => {
-    clickedKey.classList.remove("active");
-  }, 150);
-};
+let audioMap = {};
 
 pianoKeys.forEach((key) => {
+  const keyName = key.dataset.key;
+  audioMap[keyName] = new Audio(`src/tunes/${keyName}.wav`);
+
+
+  key.addEventListener("mousedown", () => {
+    key.classList.add("active");
+    audioMap[keyName].currentTime = 3;
+    audioMap[keyName].play();
+  });
+
+  key.addEventListener("mouseup", () => {
+    key.classList.remove("active");
+    audioMap[keyName].pause();
+  });
+
+  key.addEventListener("mouseleave", () => {
+    key.classList.remove("active");
+    audioMap[keyName].pause();
+  });
+
   key.addEventListener("click", () => playTune(key.dataset.key));
   mapedKeys.push(key.dataset.key);
 });
@@ -24,17 +33,45 @@ pianoKeys.forEach((key) => {
 document.addEventListener("keydown", (e) => {
   if (mapedKeys.includes(e.key)) {
     playTune(e.key);
+    audioMap[e.key].isPlaying = true;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    audioMap[e.key].pause();
+    audioMap[e.key].isPlaying = false;
   }
 });
 
 const handleVolume = (e) => {
-  audio.volume = e.target.value;
+  for (const key in audioMap) {
+    audioMap[key].volume = e.target.value;
+  }
 };
 
 const showHideKeys = () => {
   pianoKeys.forEach((key) => key.classList.toggle("hide"));
 };
 
+$(document).ready(function () {
+  $('.switcher').click(function () {
+    $('.switcher__button').toggleClass('switcher__button-before');
+    $('.switcher').toggleClass('switcher-before');
+  });
+});
+
 volumeSlider.addEventListener("input", handleVolume);
 
 keysCheck.addEventListener("click", showHideKeys);
+
+function playTune(key) {
+  audioMap[key].currentTime = 0;
+  audioMap[key].play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setInterval(() => {
+    clickedKey.classList.remove("active");
+  },500);
+}
